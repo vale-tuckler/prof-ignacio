47631467827c7d1ab2ed75d2dd61ff578f92f2d7
Valerie Tuckler <valeriert2011@gmail.com>
2020-10-03 22:46:50 -0500

    Fixed 2 out of 9 bugs in HomeContent.js, all animations are complete with the exception of HomeContent.js

--- src/Components/HomeContent.js
+++ src/Components/HomeContent.js
@@ -1,42 +1,55 @@
 import React, { useEffect, useRef }from 'react';
-import { Power3, Power1 } from 'gsap/dist/gsap';
-import { gsap } from 'gsap/dist/gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { Power3 } from 'gsap';
+import { gsap } from 'gsap';
 import '../styles/inicio.css';    
 
-gsap.registerPlugin(ScrollTrigger);
-
-const HomeCard = props =>{        
+const HomeCard = (props) =>{        
         
         let ImgRef = useRef(null);
-        let Img2 = useRef(null);
+        //let Img2 = useRef(null);
         let colorEl = useRef(null); //colorEl means Color Element
         let Banner1 = useRef(null); //Banner makes reference to the phrase at the beginning of #section4 which is above the flag
         let Banner2 = useRef(null);
+        let Banner = useRef(null);
 
         function HomeAnimation(){
 
-           console.log("After"+"\nBanner 1 = " + Banner1 + " \nBanner 2 = "+ Banner2);         
+           //console.log("After"+"\nBanner 1 = " + Banner1 + " \nBanner 2 = "+ Banner2);         
+            console.log(ImgRef);
+            console.log(colorEl);
+            //console.log(Banner);
+           console.log(Banner1);
+            console.log(Banner2);
 
-            let Bounce = gsap.parseEase("bounce.out");
-            let Circ = gsap.parseEase("circ.out");
-
+            /*let Bounce = gsap.parseEase("bounce.out");
+            let Circ = gsap.parseEase("circ.out");*/
             let mytimeline = gsap.timeline({delay:2});
+            
             mytimeline
-            .fromTo([ImgRef.current, Img2.current],
-                {opacity:0, xPercent:-8, visibility:"hidden"},
-                {duration:2, visibility:"visible", opacity:1, xPercent:0, ease:Power3.easeInOut, scrollTrigger:{start:ImgRef, start:"top bottom"}})
-            .fromTo(colorEl.current,
-                {opacity:0, xPercent:8, visibility:"hidden"},
-                {duration:1.5, visibility:"visible",opacity:1,xPercent:0, ease:Power3.easeOut})
-            .fromTo(Banner1.current,
-                {opacity:0, yPercent:12, visibility:"hidden"},
-                {duration:1.5, visibility:"visible",opacity:1,yPercent:0, ease:Bounce})
-            .fromTo(Banner2.current,
-                {opacity:0, xPercent:12, visibility:"hidden"},
-                {duration:1.5, visibility:"visible", opacity:1, xPercent:0, ease:Circ})
+                .fromTo(ImgRef,
+                    {opacity:0, xPercent:-8, visibility:"hidden"},
+                    {duration:2, visibility:"visible", opacity:1, xPercent:0, ease:Power3.easeInOut})
+                    /* 
+                    .fromTo([ImgRef, Img2],
+                    {opacity:0, xPercent:-8, visibility:"hidden"},
+                    {duration:2, visibility:"visible", opacity:1, xPercent:0, ease:Power3.easeInOut})
+                    */
+                .fromTo(colorEl,
+                    {opacity:0, xPercent:8, visibility:"hidden"},
+                    {duration:1.5, visibility:"visible",opacity:1,xPercent:0, ease:Power3.easeOut})
                 
-                return mytimeline;
+                /*.fromTo(Banner,
+                    {opacity:0, yPercent:12, visibility:"hidden"},
+                    {duration:1.5, visibility:"visible",opacity:1,yPercent:0, ease:Power3.easeOut})
+                */
+                .fromTo(Banner1,
+                    {opacity:0, yPercent:12, visibility:"hidden"},
+                    {duration:1.5, visibility:"visible",opacity:1,yPercent:0, ease:Power3.easeOut})
+                .fromTo(Banner2,
+                    {opacity:0, xPercent:12, visibility:"hidden"},
+                    {duration:1.5, visibility:"visible", opacity:1, xPercent:0, ease:Power3.easeOut})
+                                    
+            return mytimeline;
         }
         
         useEffect(() => {                                      
@@ -48,11 +61,11 @@ const HomeCard = props =>{
             return(
                 <div className="Homecontainer">                
             <section id={props.sectionID}>
-                    <div className="phrase4" id = "phrase-container">
-                            <p className="banner" ref = {el => Banner1 = el}>{props.text1}</p>                            
-                            <p className="banner"ref = {el => Banner2 = el}> {props.text2}</p>                            
+                    <div className="phrase4" id = "phrase-container" ref={ y => colorEl = y}>
+                            <p className="banner" ref = {a => Banner1 = a}>{props.text1}</p>                            
+                            <p className="banner"ref = {b => Banner2 = b}> {props.text2}</p>                            
 
-                            <img src = {props.image} alt ={props.alt} title={props.title} ref={i => Img2 = i} id="flags"/>
+                            <img src = {props.image} alt ={props.alt} title={props.title} ref={i => ImgRef = i} id="flags"/>
                             <aside className="left">                            
                                 <img src={props.Star} alt="Estrella" title="Estrella" className="izq" id="izq1"/>
                                 <img src={props.Star} alt="Estrella" title="Estrella" className="izq" id="izq2"/>
@@ -82,7 +95,7 @@ const HomeCard = props =>{
                         <img src = {props.image} alt ={props.alt} title={props.title} ref={i => ImgRef = i}/>
                     </div>
                     <div className="phrase">
-                        <aside ref= {c => colorEl = c} id={props.asideID}>                            
+                        <aside ref= {x => colorEl = x} id={props.asideID}>                            
                             <p className="text">{props.text1}</p>                            
                             <p className="text"> {props.text2}</p>
                         </aside>
